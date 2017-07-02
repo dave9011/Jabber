241e923c741e3e000fd53edd98c97ced5e37570d
Dave <hrdavidl@gmail.com>
2017-07-02 19:30:14 -0400

    Typing indicator now works, Audio plays when user sends a message, Modified Styles,

--- jabber/src/components/Chat.vue
+++ jabber/src/components/Chat.vue
@@ -7,13 +7,18 @@ export default {
     name: 'chat',
     data () {
 	    return {
+	        isOtherUserTyping: false,
 	        messages: [],
+	        newMessage: '',
+	        notification: null,
 	        statuses: {
                 OFFLINE: 'Offline',
                 ONLINE: 'Online'
 	        },
 	        status: null,
-            socket: null
+            socket: null,
+            typingIndicator: '',
+            users: []
 	    }
     },
     mounted: function() {
@@ -36,8 +41,8 @@ export default {
             if (self.socket !== undefined) {
                 self.setStatus(self.statuses.ONLINE);
 
-                var notification = new Audio('../static/notif.mp3');
-                notification.volume = 0.1;
+                self.notification = new Audio('../static/notif.mp3');
+                self.notification.volume = 0.1;
 
                 //Notify when user connects
                 self.socket.on('connect', function(){
@@ -45,9 +50,54 @@ export default {
                     self.socket.emit('userJoined', { name : 'name here!'});
                 });
 
+                self.socket.on('updateUsersConnected', function (data) {
+                    if (data.length) {
+                        self.users = data;
+                    }
+                });
+
                 //Listen for output
                 self.socket.on('output', function (data) {
                     self.messages = data;
+                });
+
+                self.socket.on('updateTyping', function(data) {
+                    self.typingIndicator = data.name + " ...";
+                    self.isOtherUserTyping = true;
+
+                    var timeout = setTimeout( function(){
+                        self.typingIndicator = '';
+                        clearInterval(timeout);
+                    }, 1300);
+                });
+            }
+        },
+        isOwnMessage: function (message) {
+            return message.name == 'Dave';
+        },
+        onNewMessageSubmit: function (event) {
+            this.newMessage = this.newMessage.trim();
+
+            var self = this;
+            var userName = 'David'; // TODO: udpate this to show current user's name
+
+            if(event.which === 13 && event.shiftKey === false){
+                event.preventDefault();
+                var date = new Date();
+                var time = date.getHours()%12 + ":" + (date.getMinutes()<10?'0':'') + date.getMinutes();
+
+                self.socket.emit('input', {
+                    name : userName,
+                    message : self.value,
+                    time : time
+                });
+
+                console.log(self.notification);
+
+                self.notification.play();
+            } else {
+                self.socket.emit('typing', {
+                    name : userName
                 });
             }
         },
